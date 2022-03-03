d202f752ec10802e60a31ab749d59bb4f58e9225
Adam Pratley <32518070+AdamPratley@users.noreply.github.com>
2022-03-03 12:37:07 +0000

    Initial Commit

---
+++ client.js
@@ -0,0 +1,132 @@
+// peer connection
+var pc = null;
+
+// data channel
+var dc = null, dcInterval = null;
+
+function createPeerConnection() {
+    var config = {
+        sdpSemantics: 'unified-plan'
+    };
+
+    pc = new RTCPeerConnection(config);
+    
+    // connect audio / video
+    pc.addEventListener('track', function(evt) {
+        if (evt.track.kind == 'video')
+            document.getElementById('video').srcObject = evt.streams[0];
+        else
+            document.getElementById('audio').srcObject = evt.streams[0];
+    });
+
+    return pc;
+}
+
+function negotiate() {
+    return pc.createOffer().then(function(offer) {
+        return pc.setLocalDescription(offer);
+    }).then(function() {
+        // wait for ICE gathering to complete
+        return new Promise(function(resolve) {
+            if (pc.iceGatheringState === 'complete') {
+                resolve();
+            } else {
+                function checkState() {
+                    if (pc.iceGatheringState === 'complete') {
+                        pc.removeEventListener('icegatheringstatechange', checkState);
+                        resolve();
+                    }
+                }
+                pc.addEventListener('icegatheringstatechange', checkState);
+            }
+        });
+    }).then(function() {
+        var offer = pc.localDescription;
+
+        return fetch('/offer', {
+            body: JSON.stringify({
+                sdp: offer.sdp,
+                type: offer.type,
+                bit_rate: document.getElementById('bit_rate').value
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+        });
+    }).then(function(response) {
+        return response.json();
+    }).then(function(answer) {
+        return pc.setRemoteDescription(answer);
+    }).catch(function(e) {
+        alert(e);
+    });
+}
+
+function start() {
+    document.getElementById('start').style.display = 'none';
+
+    pc = createPeerConnection();
+
+    var constraints = {
+        audio: false,
+        video: false
+    };
+    
+    var resolution = document.getElementById('video-resolution').value;
+    if (resolution) {
+        resolution = resolution.split('x');
+        constraints.video = {
+            width: parseInt(resolution[0], 0),
+            height: parseInt(resolution[1], 0)
+        };
+    } else {
+        constraints.video = true;
+    }
+
+    if (constraints.audio || constraints.video) {
+        if (constraints.video) {
+            document.getElementById('media').style.display = 'block';
+        }
+        navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+            stream.getTracks().forEach(function(track) {
+                pc.addTrack(track, stream);
+            });
+            return negotiate();
+        }, function(err) {
+            alert('Could not acquire media: ' + err);
+        });
+    } else {
+        negotiate();
+    }
+
+    document.getElementById('stop').style.display = 'inline-block';
+}
+
+function stop() {
+    document.getElementById('stop').style.display = 'none';
+
+    // close data channel
+    if (dc) {
+        dc.close();
+    }
+
+    // close transceivers
+    if (pc.getTransceivers) {
+        pc.getTransceivers().forEach(function(transceiver) {
+            if (transceiver.stop) {
+                transceiver.stop();
+            }
+        });
+    }
+
+    // close local audio / video
+    pc.getSenders().forEach(function(sender) {
+        sender.track.stop();
+    });
+
+    // close peer connection
+    setTimeout(function() {
+        pc.close();
+    }, 500);
+}
