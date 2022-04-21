53f1f37b196d8e6eec099ebf0e26e09f24028350
Adam Pratley <32518070+AdamPratley@users.noreply.github.com>
2022-04-21 17:36:49 +0100

    statically serving libs directory
    added empty favicon file to suppress error
    
    update index.html and app.js to statically serve the libraries directory

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use(express.static('libs'));
+
 app.get('/', function (req, res) {
     res.sendFile('index.html', {root: __dirname});
 });
@@ -10,18 +12,13 @@ app.get('/main.js', function (req, res) {
     res.sendFile('main.js', {root: __dirname});
 });
 
-app.get('/libs/Chart.min.js', function (req, res) {
-    res.sendFile('/libs/Chart.min.js', {root: __dirname});
-});
-
-app.get('/libs/dash.all.min.js', function (req, res) {
-    res.sendFile('/libs/dash.all.min.js', {root: __dirname});
+app.get('/favicon.ico', function (req, res) {
+    res.sendFile('/favicon.ico', {root: __dirname});
 });
 
 app.get('/style.css', function (req, res) {
     res.sendFile('style.css', {root: __dirname});
 });
-
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
