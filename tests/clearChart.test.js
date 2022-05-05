a7812ad96040ae53411bcc2af895638088ac34ae
Adam Pratley <32518070+AdamPratley@users.noreply.github.com>
2022-05-05 18:03:07 +0100

    100% Test Coverage
    Fixed clearChart.test.js to test both branches

--- tests/clearChart.test.js
+++ tests/clearChart.test.js
@@ -1,11 +1,21 @@
 /** @jest-environment jsdom */
 const clearChart = require('../src/clearChart');
 
-class mockClass{
-    destroy = function(){};
+const mockClass = {
+    destroy: jest.fn()
 }
-const dataChart = new mockClass;
 
-test('Test clearChart', function(){
-    expect(clearChart(dataChart));
-});
+const dataChart = mockClass;
+
+describe('Test clearChart', function(){
+    test('dataChart != null', function(){
+        clearChart(dataChart)
+        expect(mockClass.destroy.mock.calls.length).toBe(1);
+    });
+    
+    test('dataChart == null', function(){
+        clearChart(null)
+        expect(mockClass.destroy.mock.calls.length).toBe(1);
+    });
+})
+
